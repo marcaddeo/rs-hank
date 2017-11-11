04fcdde9611d698330c2c1a2a1eb1915e6bd2107
Marc Addeo <marcaddeo@gmail.com>
2017-11-11 18:40:54 -0500

    Bubble errors to caller

--- src/handlers.rs
+++ src/handlers.rs
@@ -36,30 +36,30 @@ impl<'a> HandlerContext<'a> {
 }
 
 pub fn nop_handler(context: &HandlerContext) -> Result<()> {
-    let re = Regex::new(r"^.nop$").unwrap();
+    let re = Regex::new(r"^.nop$")?;
 
     if re.is_match(context.message) {
-        context.server.send_privmsg(context.target, "nop pls").unwrap();
+        context.server.send_privmsg(context.target, "nop pls")?;
     }
 
     Ok(())
 }
 
 pub fn nm_handler(context: &HandlerContext) -> Result<()> {
-  let re = Regex::new(r"^nmu$").unwrap();
+  let re = Regex::new(r"^nmu$")?;
 
   if re.is_match(context.message) {
-    context.server.send_privmsg(context.target, "nm").unwrap();
+    context.server.send_privmsg(context.target, "nm")?;
   }
 
   Ok(())
 }
 
 pub fn maize_handler(context: &HandlerContext) -> Result<()> {
-    let re = Regex::new(r"^[o]+[h]+$").unwrap();
+    let re = Regex::new(r"^[o]+[h]+$")?;
 
     if re.is_match(context.message) {
-        context.server.send_privmsg(context.target, "maize").unwrap();
+        context.server.send_privmsg(context.target, "maize")?;
     }
 
     Ok(())
@@ -68,7 +68,7 @@ pub fn maize_handler(context: &HandlerContext) -> Result<()> {
 pub fn hi_handler(context: &HandlerContext) -> Result<()> {
     let re = Regex::new(
         &format!(r"(?i)h(i?) {}", context.server.config().nickname())
-    ).unwrap();
+    )?;
     let greetings = vec![
         "hi",
         "h",
@@ -81,11 +81,11 @@ pub fn hi_handler(context: &HandlerContext) -> Result<()> {
     ];
 
     if re.is_match(context.message) {
-        let greeting = rand::thread_rng().choose(&greetings).unwrap();
+        let greeting = rand::thread_rng().choose(&greetings).ok_or("")?;
         context.server.send_privmsg(
             context.target,
             &format!("{} {}", greeting, context.sender)
-        ).unwrap();
+        )?;
     }
 
     Ok(())
@@ -94,7 +94,7 @@ pub fn hi_handler(context: &HandlerContext) -> Result<()> {
 pub fn youtube_handler(context: &HandlerContext) -> Result<()> {
     let re = Regex::new(
         r"^.*((youtu.be/)|(v/)|(/u/\w/)|(embed/)|(watch\?))\??v?=?(?P<video_id>[^#\&\?\s]*).*"
-    ).unwrap();
+    )?;
 
     let captures = match re.captures(context.message) {
         Some(captures) => captures,
@@ -106,26 +106,26 @@ pub fn youtube_handler(context: &HandlerContext) -> Result<()> {
     let url = format!(
         "https://www.googleapis.com/youtube/v3/videos?part=contentDetails,snippet,statistics&id={video_id}&key={api_key}",
         video_id = &captures["video_id"],
-        api_key = env::var("HANK_YOUTUBE_API_KEY").unwrap(),
+        api_key = env::var("HANK_YOUTUBE_API_KEY")?,
     );
-    easy.url(&url).unwrap();
+    easy.url(&url)?;
     {
         let mut transfer = easy.transfer();
         transfer.write_function(|new_data| {
             data.extend_from_slice(new_data);
             Ok(new_data.len())
-        }).unwrap();
-        transfer.perform().unwrap();
+        })?;
+        transfer.perform()?;
     }
-    let body = String::from_utf8(data.to_vec()).unwrap();
-    let json: Value = serde_json::from_str(&body).unwrap();
+    let body = String::from_utf8(data.to_vec())?;
+    let json: Value = serde_json::from_str(&body)?;
     let video = &json["items"][0];
-    let video_title: String = serde_json::from_value(video["snippet"]["title"].clone()).unwrap();
-    let video_duration: String = serde_json::from_value(video["contentDetails"]["duration"].clone()).unwrap();
-    let video_definition: String = serde_json::from_value(video["contentDetails"]["definition"].clone()).unwrap();
-    let video_views: String = serde_json::from_value(video["statistics"]["viewCount"].clone()).unwrap();
-    let video_likes: String = serde_json::from_value(video["statistics"]["likeCount"].clone()).unwrap();
-    let video_dislikes: String = serde_json::from_value(video["statistics"]["dislikeCount"].clone()).unwrap();
+    let video_title: String = serde_json::from_value(video["snippet"]["title"].clone())?;
+    let video_duration: String = serde_json::from_value(video["contentDetails"]["duration"].clone())?;
+    let video_definition: String = serde_json::from_value(video["contentDetails"]["definition"].clone())?;
+    let video_views: String = serde_json::from_value(video["statistics"]["viewCount"].clone())?;
+    let video_likes: String = serde_json::from_value(video["statistics"]["likeCount"].clone())?;
+    let video_dislikes: String = serde_json::from_value(video["statistics"]["dislikeCount"].clone())?;
 
     let duration = parse_duration(&video_duration).unwrap();
     let tm = time::empty_tm() + duration;
@@ -133,7 +133,7 @@ pub fn youtube_handler(context: &HandlerContext) -> Result<()> {
     let message = format!(
         "{title} [{duration}] {definition} {views} views (+{likes}|-{dislikes}) {permalink}",
         title = format!("\x02{}\x0F", video_title),
-        duration = tm.strftime("%X").unwrap(),
+        duration = tm.strftime("%X")?,
         definition = format!("\x02\x0304{}\x0F", video_definition.to_uppercase()),
         views = video_views,
         likes = format!("\x0303{}\x0F", video_likes),
@@ -141,7 +141,7 @@ pub fn youtube_handler(context: &HandlerContext) -> Result<()> {
         permalink = format!("\x02https://youtu.be/{}\x0F", &captures["video_id"]),
     );
 
-    context.server.send_privmsg(context.target, &message).unwrap();
+    context.server.send_privmsg(context.target, &message)?;
 
     Ok(())
 }
@@ -159,7 +159,7 @@ pub fn rejoin_handler(context: &HandlerContext) -> Result<()> {
 }
 
 pub fn btc_handler(context: &HandlerContext) -> Result<()> {
-    let re = Regex::new(r"^.btc$").unwrap();
+    let re = Regex::new(r"^.btc$")?;
 
     if !re.is_match(context.message) {
         return Ok(());
@@ -167,25 +167,25 @@ pub fn btc_handler(context: &HandlerContext) -> Result<()> {
 
     let mut data = Vec::new();
     let mut easy = Easy::new();
-    easy.url("https://blockchain.info/ticker").unwrap();
+    easy.url("https://blockchain.info/ticker")?;
     {
         let mut transfer = easy.transfer();
         transfer.write_function(|new_data| {
             data.extend_from_slice(new_data);
             Ok(new_data.len())
-        }).unwrap();
-        transfer.perform().unwrap();
+        })?;
+        transfer.perform()?;
     }
-    let body = String::from_utf8(data.to_vec()).unwrap();
-    let json: Value = serde_json::from_str(&body).unwrap();
+    let body = String::from_utf8(data.to_vec())?;
+    let json: Value = serde_json::from_str(&body)?;
 
     let message = format!(
         "{}{:.3}",
-        json["USD"]["symbol"].as_str().unwrap(),
+        json["USD"]["symbol"].as_str().ok_or("")?,
         json["USD"]["last"],
     );
 
-    context.server.send_privmsg(context.target, &message).unwrap();
+    context.server.send_privmsg(context.target, &message)?;
 
     Ok(())
 }
