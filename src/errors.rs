18dc71a0ae403e746e0dc53b92307333f2eae05e
Marc Addeo <marcaddeo@gmail.com>
2017-11-11 18:40:54 -0500

    Bubble errors to caller

--- src/errors.rs
+++ src/errors.rs
@@ -1,1 +1,11 @@
-error_chain! { }
+error_chain! {
+    foreign_links {
+        IrcError(::irc::error::Error);
+        RegexError(::regex::Error);
+        EnvVarError(::std::env::VarError);
+        CurlError(::curl::Error);
+        StringUtf8Error(::std::string::FromUtf8Error);
+        SerdeJsonError(::serde_json::Error);
+        TimeError(::time::ParseError);
+    }
+}
